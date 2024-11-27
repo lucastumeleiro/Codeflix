db86b5c6a29155f98988ba32fe53f0f553c91b91
Lucas Tumeleiro <lucasftumeleiro@gmail.com>
2024-11-26 23:37:57 -0300

    feat: Pagination e Gateway category

---
+++ catalogo-admin/domain/src/main/java/com/codeflix/admin/catalogo/domain/category/CategoryGateway.java
@@ -0,0 +1,18 @@
+package com.codeflix.admin.catalogo.domain.category;
+
+import com.codeflix.admin.catalogo.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface CategoryGateway {
+
+    Category create(Category category);
+
+    Category deleteById(CategoryID id);
+
+    Optional<Category> findById(CategoryID id);
+
+    Pagination<Category> findAll(CategorySearchQuery query);
+
+    Category update(Category category);
+}
