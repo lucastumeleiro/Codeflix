d3fa5ecbd88375df87caa2823357ffa0eed5b059
Lucas Tumeleiro <lucasftumeleiro@gmail.com>
2024-12-28 18:08:10 -0300

    feat: infrastructure - teste integração Get Category

---
+++ catalogo-admin/infrastructure/src/test/java/com/codeflix/admin/catalogo/application/category/retrieve/get/GetCategoryUseCaseIT.java
@@ -0,0 +1,75 @@
+package com.codeflix.admin.catalogo.application.category.retrieve.get;
+
+import com.codeflix.admin.catalogo.IntegrationTest;
+import com.codeflix.admin.catalogo.domain.category.Category;
+import com.codeflix.admin.catalogo.domain.category.CategoryGateway;
+import com.codeflix.admin.catalogo.domain.category.CategoryID;
+import com.codeflix.admin.catalogo.domain.exceptions.DomainException;
+import com.codeflix.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.codeflix.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
+
+import java.util.Arrays;
+
+@IntegrationTest
+public class GetCategoryUseCaseIT {
+
+    @Autowired
+    private GetCategoryUseCase useCase;
+    @Autowired
+    private CategoryRepository repository;
+
+    @MockitoSpyBean
+    private CategoryGateway gateway;
+
+    @BeforeEach
+    void cleanUp() {
+        repository.deleteAll();
+    }
+
+    @Test
+    public void givenValidId_whenCallsGetCategory_shouldReturnCategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var category = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var expectedId = category.getId();
+
+        save(category);
+
+        final var actualCategory = useCase.execute(expectedId.getValue());
+
+        Assertions.assertEquals(expectedId, actualCategory.id());
+        Assertions.assertEquals(expectedName, actualCategory.name());
+        Assertions.assertEquals(expectedDescription, actualCategory.description());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertEquals(category.getCreatedAt(), actualCategory.createdAt());
+        Assertions.assertEquals(category.getUpdatedAt(), actualCategory.updatedAt());
+        Assertions.assertEquals(category.getDeletedAt(), actualCategory.deleteAt());
+    }
+
+    @Test
+    public void givenInvalidId_whenCallsGetCategory_shouldReturnNotFound() {
+        final var expectedId = CategoryID.from("123");
+        final var expectedErrorMessage = "Category with ID 123 was not found";
+
+        final var actualException = Assertions.assertThrows(
+                DomainException.class,
+                () -> useCase.execute(expectedId.getValue())
+        );
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+    }
+
+    private void save(final Category... category) {
+        repository.saveAllAndFlush(
+                Arrays.stream(category)
+                        .map(CategoryJpaEntity::from)
+                        .toList()
+        );
+    }
+}
