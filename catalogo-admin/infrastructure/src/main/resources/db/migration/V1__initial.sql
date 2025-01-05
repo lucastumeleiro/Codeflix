630f8069ef4e9205c7c269128e59fbc2c29fbbc0
Lucas Tumeleiro <lucasftumeleiro@gmail.com>
2025-01-05 02:35:15 -0300

    feat: infrastructure - migration genre

--- catalogo-admin/infrastructure/src/main/resources/db/migration/V1__initial.sql
+++ catalogo-admin/infrastructure/src/main/resources/db/migration/V1__initial.sql
@@ -1,5 +1,5 @@
-CREATE TABLE category (
-    id VARCHAR(36) PRIMARY KEY,
+CREATE TABLE categories (
+    id CHAR(36) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(4000),
     active BOOLEAN NOT NULL DEFAULT TRUE,
