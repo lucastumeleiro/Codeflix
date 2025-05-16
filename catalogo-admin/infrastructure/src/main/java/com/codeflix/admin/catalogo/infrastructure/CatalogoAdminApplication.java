1b8b551e29cefac805ac7e7ff8b945bf72ffe318
Lucas Tumeleiro <lucasftumeleiro@gmail.com>
2025-05-15 22:45:43 -0300

    feat: github actions - ci-cd - teste

--- catalogo-admin/infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/CatalogoAdminApplication.java
+++ catalogo-admin/infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/CatalogoAdminApplication.java
@@ -10,7 +10,6 @@ import org.springframework.core.env.AbstractEnvironment;
 public class CatalogoAdminApplication {
     public static void main(String[] args) {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
-
         SpringApplication.run(WebServerConfig.class, args);
     }
 
