53b49b998fcf116e3e0901b8ee5568b4df3e4911
Lucas Tumeleiro <lucasftumeleiro@gmail.com>
2025-05-18 03:13:32 -0300

    observabilidade - elasticsearch, kibana, longstash e filebeat

--- catalogo-admin/infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/CatalogoAdminApplication.java
+++ catalogo-admin/infrastructure/src/main/java/com/codeflix/admin/catalogo/infrastructure/CatalogoAdminApplication.java
@@ -1,6 +1,8 @@
 package com.codeflix.admin.catalogo.infrastructure;
 
 import com.codeflix.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +10,14 @@ import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class CatalogoAdminApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CatalogoAdminApplication.class);
+
     public static void main(String[] args) {
+        LOG.info("[step:to-be-init] [id:1] Inicializando o Spring");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
+        LOG.info("[step:inittialized] [id:2] Spring inicializado..");
     }
 
 }
--- catalogo-admin/sandbox/run.sh
+++
@@ -1,9 +0,0 @@
-docker network create adm_videos_services
-
-mkdir -m 777 .docker
-mkdir -m 777 .docker/keycloak
-
-docker-compose -f services/docker-compose.yml up -d
-
-echo "Inicializando os containers..."
-sleep 20
