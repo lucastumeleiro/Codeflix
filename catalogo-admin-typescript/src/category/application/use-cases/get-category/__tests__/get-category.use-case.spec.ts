092f9542dc25a36e2d34de90a8c1237a37a384bf
lucas-tumeleiro <lucasftumeleiro@gmail.com>
2025-08-10 03:08:15 -0300

    #feat - categoria - input use-cases e ajuste pastas

--- catalogo-admin-typescript/src/category/application/use-cases/get-category/__tests__/get-category.use-case.spec.ts
+++ catalogo-admin-typescript/src/category/application/use-cases/get-category/__tests__/get-category.use-case.spec.ts
@@ -5,15 +5,15 @@ import {
 } from "../../../../../shared/domain/value-objects/uuid";
 import { Category } from "../../../../domain/category.entity";
 import { CategoryInMemoryRepository } from "../../../../infra/db/in-memory/category-in-memory.repository";
-import { DeleteCategoryUseCase } from "../../delete-category.use-case";
+import { GetCategoryUseCase } from "../get-category.use-case";
 
-describe("DeleteCategoryUseCase Unit Tests", () => {
-  let useCase: DeleteCategoryUseCase;
+describe("GetCategoryUseCase Unit Tests", () => {
+  let useCase: GetCategoryUseCase;
   let repository: CategoryInMemoryRepository;
 
   beforeEach(() => {
     repository = new CategoryInMemoryRepository();
-    useCase = new DeleteCategoryUseCase(repository);
+    useCase = new GetCategoryUseCase(repository);
   });
 
   it("should throws error when entity not found", async () => {
@@ -22,18 +22,23 @@ describe("DeleteCategoryUseCase Unit Tests", () => {
     );
 
     const uuid = new Uuid();
-
     await expect(() => useCase.execute({ id: uuid.id })).rejects.toThrow(
-      new NotFoundError(uuid, Category)
+      new NotFoundError(uuid.id, Category)
     );
   });
 
-  it("should delete a category", async () => {
-    const items = [new Category({ name: "test 1" })];
+  it("should returns a category", async () => {
+    const items = [Category.create({ name: "Movie" })];
     repository.items = items;
-    await useCase.execute({
+    const spyFindById = jest.spyOn(repository, "findById");
+    const output = await useCase.execute({ id: items[0].category_id.id });
+    expect(spyFindById).toHaveBeenCalledTimes(1);
+    expect(output).toStrictEqual({
       id: items[0].category_id.id,
+      name: "Movie",
+      description: null,
+      is_active: true,
+      created_at: items[0].created_at,
     });
-    expect(repository.items).toHaveLength(0);
   });
 });
